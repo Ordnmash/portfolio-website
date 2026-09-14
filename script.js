6a597a02cefad297d8ac2c6ba0e3918b9985c618
ordnmash <ordnfxist@gmail.com>
2026-09-14 22:06:30 +0200

    Refactor event listeners, age update logic and fix endless line

--- script.js
+++ script.js
@@ -129,7 +129,7 @@ detailsbtn.addEventListener("click", function(){
   tvnames.classList.remove("notactive");
   topmenublank.classList.remove("active");
   topmenubtn.classList.add("active");
-  righttv.classList.remove("active")
+  righttv.classList.remove("active");
 });
 
 const ca = document.getElementById("current-age");
